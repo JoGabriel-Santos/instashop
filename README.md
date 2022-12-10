ecb5d00c9fd8256cab09c30b9d7b3a15d93f4562
JoGabriel-Santos <jgabriel.professional@gmail.com>
2022-12-09 22:52:58 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Instashop
+
+This project was built using the MERN Stack:
+
+MongoDB /
+Express /
+React /
+Node.js
+
+Working on it...
---
+++ server/index.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+const CONNECTION_URL = ''
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => { console.log(`Server running on port: ${PORT}`) }))
+    .catch((error) => { console.log(error.message) })
